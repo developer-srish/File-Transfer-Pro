8cece76c7df8a7cde884625df333e94ef53f0024
Srish Ghosh <srishghosh63@gmail.com>
2026-07-29 17:09:41 +0530

    Update client.py

--- client.py
+++ client.py
@@ -4,6 +4,62 @@ import builtins
 from pyfiglet import Figlet
 from rich.console import Console
 from rich.progress import Progress
+import smtplib
+from email.message import EmailMessage
+import random as r
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+se = os.getenv("EMAIL")
+app = os.getenv("APP_PASSWORD")
+console = Console()
+def print(text, style="white", delay=0.05):
+    for char in text:
+        console.print(char, style=style, end="")
+        time.sleep(delay)
+    console.print()
+with open('user.json', 'r') as f:
+    dat=json.load(f)
+if dat['Count'] == 0:
+    print(f'Welcome {dat["Name"]} to File Tarnsfer pro',style='magenta')
+    n=input('Enter your Name : ')
+    ema=input('Enter Email id')
+    
+    
+    su='Email Verification'
+    co= r.randint(100000, 999999)
+    msg=EmailMessage()
+    msg.set_content(
+    f"""Welcome to File Transfer Pro!
+
+Your verification OTP is: {co}
+
+Do not share this OTP with anyone.
+"""
+)
+    msg['Subject'] =su
+    msg['From'] =se
+    msg['To'] =ema
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(se, app)
+        server.send_message(msg)
+    oo=input('Enter otp')
+
+    if int(oo) == co :
+        dat["Count"]=1
+        dat["Name"] = n
+        dat["Email"]=ema
+        print(f'Welcome {dat["Name"]}',style='magenta')
+        with open("user.json", "w") as f:
+            json.dump(dat, f, indent=4)
+    else:
+        print("Wrong OTP!", style="red")
+        exit()
+else:
+    print(f'Welcome {dat["Name"]} to File Tarnsfer Pro')
 console = Console()
 fig = Figlet(font="starwars")
 banner = fig.renderText("File Transfer Pro")
